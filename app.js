6ceafea748800c34e8ba2f5ec1ce274128b27459
Kumar Anshuman <kanshuman6484@gmail.com>
2025-06-07 18:33:37 +0530

    trying to fix cors error

--- app.js
+++ app.js
@@ -11,12 +11,7 @@ import conversationRoutes from './routes/conversationRoutes.js'
 const app = express()
 
 // Middlewares
-app.use(cors({
-  origin: '*',
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
-  credentials: true
-}));
+app.use(cors())
 app.use(morgan('dev'))
 app.use(express.json())
 
